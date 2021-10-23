656716b668de0d3f525c691a7e0ad8963aec5de8
Fredrik <73961953+FredrikThunberg@users.noreply.github.com>
2021-10-23 15:06:18 +0200

    css, js adderades
    adderade js: read more function och smooth scroll function, css: adderades för work sektionen (read more/read less)

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
 
+// all som har klassen showmore, för varje function
+document.querySelectorAll(".showmore").forEach(function (p) {
+  p.querySelector("a").addEventListener("click", function() {
+    p.classList.toggle("show");
+    this.textContent = p.classList.contains("show") ? "Show Less" :
+    "Show More";
+  });
+});
+
+
+document.querySelectorAll("a[href^=\"#\"]").forEach((anchor) => {
+  anchor.addEventListener("click", function (ev) {
+    ev.preventDefault();
+
+    const targetElement = document.querySelector(this.getAttribute("href"));
+    targetElement.scrollIntoView({
+      block: "start",
+      alignToTop: true,
+      behavior: "smooth"
+    });
+  });
+});
